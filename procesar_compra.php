8e0e3806c31db2fb2760ca6a5a515c64f8079634
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-25 13:49:09 -0500

    Update procesar_compra.php

--- procesar_compra.php
+++ procesar_compra.php
@@ -1,1 +1,31 @@
-informacion
+<?php
+        $producto1 = $_POST['product1'];
+        $precio1 = $_POST['precio1'];
+        
+        $producto2 = $_POST['product2'];
+        $precio2 = $_POST['precio2'];
+        
+        $producto3 = $_POST['product3'];
+        $precio3 = $_POST['precio3'];
+        
+        $total = $precio1 + $precio2 + $precio3;
+        $descuento = $total * 0.16;
+        $totalPagar = $total - $descuento;
+echo "Producto1:".$product1;
+echo"<br>";
+  echo "Precio1:".$precio1;
+echo"<br>";
+  echo "Producto2:".$product2;
+echo"<br>";
+  echo "Precio2:".$precio2;
+echo"<br>";
+echo "Producto2:".$product3;
+echo"<br>";
+  echo "Precio2:".$precio3;
+echo"<br>";
+  echo "Total a pagar:".$total;
+echo"<br>";
+ echo "Descuento (16%):".$descuento;
+echo"<br>";
+   echo "Total a pagar:".$totalPagar;
+ ?>
