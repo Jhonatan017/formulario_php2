09bc773d5fec4cbb9ccb6bb16357a10b6fa38d95
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-25 15:43:28 -0500

    Create ejercicio1.php

---
+++ ejercicio1.php
@@ -0,0 +1,25 @@
+<html>
+   <head>
+  </head>
+  
+  <body>
+  <form name="Tienda" class="formulario" action="informacion.php" method="POST">
+     <table class="tabla" border="1">
+       <tr>
+          <td>PRODUCTO</td>
+             <td>PRECIO</td>
+          <tr>
+         <td>Producto1</td>
+          <td> <input type="text" name="Producto1"></td>
+       </tr>
+        <tr>
+         <td>Producto2</td>
+          <td> <input type="text" name="Product2"></td>
+       </tr>
+         <tr>
+          <td> <input type ="submit" values="Enviar Datos" class="boton"> </td> 
+        </tr>     
+     </table>   
+  </form>
+  </body>
+</html>
