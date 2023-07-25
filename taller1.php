db5c33368fdf95be04bf6b63e2328e04787897bc
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-25 15:47:43 -0500

    Update taller1.php

--- taller1.php
+++ taller1.php
@@ -1,1 +1,13 @@
+<?php
+$Producto1=$_POST ['Product1'];
+$Producto2=$_POST ['Product2'];
+$Precio=$_POST ['Precio1'];
+$Precio=$_POST ['Precio2'];
+$suma=Precio1+Precio2
+echo "el nombre de producto es: ".$product1;
+echo "el precio del producto es: ".$Precio2;
+echo "el nombre de producto es: ".$Product2;
+echo "el precio del producto es:  ".$Precio2;
+echo "el total a pagar es:  ".$suma;
+?>
 
