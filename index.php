06ebb6e2d34e76e7713f9676deb43270fde0b520
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-25 13:44:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-
+<html>
+   <head>
+ <title> Productos</title>
+  </head>
+  
+  <body>
+  <form name="Tienda" class="formulario" action="informacion.pagina" method="POST">
+     <table class="tabla" border="1">
+       <tr>
+          <td>PRODUCTO</td>
+             <td>PRECIO</td>
+          <tr>
+         <td>Producto1</td>
+          <td> <input type="text" name="Producto1"></td>
+       </tr>
+        <tr>
+         <td>Producto2</td>
+          <td> <input type="text" name="Product2"></td>
+         <tr>
+         <td>Producto3</td>
+          <td> <input type="text" name="Product3"></td>
+       </tr>
+         <tr>
+          <td> <input type ="submit" values="Enviar Datos" class="boton"> </td> 
+        </tr>     
+     </table>   
+  </form>
+  </body>
+</html>
