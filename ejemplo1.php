ee8f70554aba0a5f2266992d5bc4f7272052e266
Jhonatan017 <128522476+Jhonatan017@users.noreply.github.com>
2023-07-25 15:52:07 -0500

    Create ejemplo1.php

---
+++ ejemplo1.php
@@ -0,0 +1,23 @@
+<html>
+   <head>
+  </head>
+  
+  <body>
+  <form name="empleados" class="formulario" action="informacion.php" method="POST">
+     <table class="tabla" border="1">
+       <tr>
+         <td>Identificacion Empleado</td>
+          <td> <input type="text" name="Identificacion"</td>
+       </tr>
+        <tr>
+         <td>Nombres Apellidos</td>
+          <td> <input type="text" name="NomApellidos"</td>
+       </tr>
+        <tr>
+         <td>Cargo a Desempeñar</td>
+          <td> <input type="text" name="Cargo"</td>
+       </tr>
+     </table>   
+  </form>
+  </body>
+</html>
